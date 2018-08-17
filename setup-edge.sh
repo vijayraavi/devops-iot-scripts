b2b1422b4534203d423691db4dbf96f894b2080b
michaeljqzq <michaeljqzq@gmail.com>
2018-08-17 12:45:58 +0800

    Add shell script to setup edge runtime

---
+++ setup-edge.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > ./microsoft-prod.list
+cp ./microsoft-prod.list /etc/apt/sources.list.d/
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
+apt-get update
+apt-get install -y moby-engine
+apt-get install -y moby-cli
+apt-get install -y iotedge
+sed -i.bak "s/device_connection_string:.*$/device_connection_string: $1/" /etc/iotedge/config.yaml
+systemctl restart iotedge
