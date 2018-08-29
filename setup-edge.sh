01129aecc4bfd9e02f12dfd3c7d0699f21c01f75
Zhiqing Qiu <michaeljqzq@users.noreply.github.com>
2018-08-29 14:21:12 +0800

    Update setup-edge.sh

--- setup-edge.sh
+++ setup-edge.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # Or it will block next scripts.
 DEBIAN_FRONTEND=noninteractive
+echo "TEST" >> /home/zhqqi/test
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > ./microsoft-prod.list
 cp ./microsoft-prod.list /etc/apt/sources.list.d/
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
 apt-get update
+apt-get install -y dialog apt-utils
 apt-get install -y moby-engine
 apt-get install -y moby-cli
 apt-get install -y iotedge
