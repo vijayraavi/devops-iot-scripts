c706f501b49245de4fb2dd1779b61adcfbe45168
Zhiqing Qiu <michaeljqzq@users.noreply.github.com>
2018-08-29 15:26:26 +0800

    Update setup-edge.sh

--- setup-edge.sh
+++ setup-edge.sh
@@ -6,16 +6,12 @@ cp ./microsoft-prod.list /etc/apt/sources.list.d/
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
 apt-get update
-apt-get install -y dialog apt-utils
 apt-get install -y moby-engine
 apt-get install -y moby-cli
-sleep 5
 echo "TEST1" >> /home/zhqqi/test
-apt-get install -y iotedge
-sleep 5
+apt-get install -y iotedge || true
 echo "TEST2" >> /home/zhqqi/test
 sed -i.bak "s|device_connection_string:.*$|device_connection_string: \"$1\"|" /etc/iotedge/config.yaml
-sleep 5
 echo "TEST3" >> /home/zhqqi/test
-systemctl restart iotedge
+systemctl restart iotedge || true
 echo "TEST4" >> /home/zhqqi/test
