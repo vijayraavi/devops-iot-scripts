5648322de7e55bb022a6a03567f3f9e38b9bbce1
Zhiqing Qiu <michaeljqzq@users.noreply.github.com>
2018-08-29 13:37:53 +0800

    Update setup-edge.sh

--- setup-edge.sh
+++ setup-edge.sh
@@ -6,6 +6,8 @@ cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
 apt-get update
 apt-get install -y moby-engine
 apt-get install -y moby-cli
+# https://www.rxx0.com/software/ubuntu/debconf-unable-to-initialize-frontend-dialog-solution.html Or it will block next scripts.
+apt-get install -y dialog
 apt-get install -y iotedge
 sed -i.bak "s|device_connection_string:.*$|device_connection_string: \"$1\"|" /etc/iotedge/config.yaml
 systemctl restart iotedge
